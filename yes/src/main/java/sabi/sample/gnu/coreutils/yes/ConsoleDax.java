08a68e82860f7cf12eb69a06b7c8df4e2a847a99
sttk <sttk.xslet@gmail.com>
2023-01-10 23:41:54 +0900

    fix:(review) modified license year and project name

--- yes/src/main/java/sabi/sample/gnu/coreutils/yes/ConsoleDax.java
+++ yes/src/main/java/sabi/sample/gnu/coreutils/yes/ConsoleDax.java
@@ -32,7 +32,7 @@ public interface ConsoleDax extends Dax, YesDax {
   @Override
   default void printHelp() {
     System.out.print("yes 1.0\n" +
-      "Copyright (C) 2022 sttk-go project.\n" +
+      "Copyright (C) 2023 sttk-java project.\n" +
       "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n" +
       "This is free software: you are free to change and redistribute it.\n" +
       "There is NO WARRANTY, to the extent permitted by law.\n" +
