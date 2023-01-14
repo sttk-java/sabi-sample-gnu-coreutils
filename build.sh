579635fb0aa4af2d7ba15425ab08d45b861dde10
Takayuki Sato <sttk.xslet@gmail.com>
2023-01-15 00:10:54 +0900

    chore: modified build.sh to stop if error (#2)

--- build.sh
+++ build.sh
@@ -1,76 +1,98 @@
 #!/usr/bin/env bash
 
+end() {
+  exitcd=$1
+  if [[ "$exitcd" != "0" ]]; then
+    exit $exitcd
+  fi
+}
+
 clean() {
   mvn clean
+  end $?
 }
 
 compile() {
   mvn compile
+  end $?
 }
 
 test() {
   mvn test
+  end $?
 }
 
 jar() {
   mvn package
+  end $?
 }
 
 javadoc() {
   mvn javadoc:javadoc
+  end $?
 }
 
 deps() {
   mvn versions:deplay-dependency-updates
+  end $?
 }
 
 sver() {
   serialver -classpath target/classes $1
+  end $?
 }
 
 native_compile() {
   mvn -Pnative package
+  end $?
 }
 
 native_test() {
   mvn -Pnative test
+  end $?
 }
 
-case "$1" in
-clean)
-  clean
-  ;;
-compile)
-  compile
-  ;;
-test)
-  test
-  ;;
-jar)
-  jar
-  ;;
-javadoc)
-  javadoc
-  ;;
-deps)
-  deps
-  ;;
-sver)
-  sver
-  ;;
-native)
-  case "$2" in
-  test)
-    native_test
+for a in "$@"; do
+  case "$a" in
+  clean)
+    clean
     ;;
-  compile | '')
-    native_compile
+  compile)
+    compile
+    ;;
+  test)
+    test
+    ;;
+  jar)
+    jar
+    ;;
+  javadoc)
+    javadoc
+    ;;
+  deps)
+    deps
+    ;;
+  sver)
+    sver
+    ;;
+  native)
+    case "$2" in
+    test)
+      native_test
+      ;;
+    compile | '')
+      native_compile
+      ;;
+    esac
+    ;;
+  '')
+    clean
+    jar
+    javadoc
+    ;;
+  *)
+    echo "Bad task: $a"
+    exit 1
     ;;
   esac
-  ;;
-'')
-  clean
-  jar
-  javadoc
-  ;;
-esac
+done
